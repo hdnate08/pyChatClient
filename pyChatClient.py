d55239bf80403005e473bfa225386ebdcec760aa
nharris <nate.harris60@gmail.com>
2023-07-17 21:22:53 -0500

    Setting up button connections and their functions

--- pyChatClient.py
+++ pyChatClient.py
@@ -17,6 +17,28 @@ class pyChatClient:
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.window)
 
+        # Connect button signals to their respective functions
+        self.ui.login_pushButton.clicked.connect(self.login)
+        self.ui.register_pushButton.clicked.connect(self.register)
+        self.ui.exit_pushButton.clicked.connect(self.exit)
+        self.ui.sendMessage_pushButton.clicked.connect(self.sendMessage)
+        self.ui.clearMessage_pushButton.clicked.connect(self.clearMessage)
+
+    def login(self):
+        self.ui.chatLog_textBrowser.append('Inside of the login function')
+
+    def register(self):
+        self.ui.chatLog_textBrowser.append('Inside of the register function')
+
+    def exit(self):
+        self.ui.chatLog_textBrowser.append('Inside of the exit function')
+
+    def sendMessage(self):
+        self.ui.chatLog_textBrowser.append('Inside of the sendMessage function')
+
+    def clearMessage(self):
+        self.ui.chatLog_textBrowser.append('Inside of the clearMessage function')
+
     def run(self):
         self.window.show()
         self.app.exec_()
