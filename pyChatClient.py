dc3c6d41ce10442004c4151825d175660b8b98a1
nharris <nate.harris60@gmail.com>
2023-07-18 21:06:52 -0500

    Working on client to server connection. Can't get client to connect just yet and not sure if its a client issue or server issue. Currently have this error: OSError: [WinError 10049] The requested address is not valid in its context.

--- pyChatClient.py
+++ pyChatClient.py
@@ -3,5 +3,26 @@ pyChat application using PyQt5
 Nathan Harris
 July 17, 2023
 """
+import socket
 
-# Will be the pyChat client file
+
+class PyChatClient:
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.client_socket = None
+
+    def connect(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.client_socket.connect((self.server_address, self.server_port))
+            print("Connected to the server.")
+        except ConnectionRefusedError:
+            print("Connection refused. Make sure the server is running.")
+
+    def close(self):
+        if self.client_socket is None:
+            return
+        self.client_socket.close()
+        print("Connection closed.")
--- pyChatClientBackend.py
+++ pyChatClientBackend.py
@@ -6,6 +6,7 @@ July 17, 2023
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from pyChat_loginScreen_GUI import Ui_loginScreen_MainWindow
+from pyChatClient import PyChatClient
 import time
 
 
@@ -13,6 +14,7 @@ class pyChatClientBackend:
     def __init__(self):
         # Defaults setup
         self.current_datetime = time.ctime()
+        self.pychat_client = PyChatClient('0.0.0.0', 60002)
 
         # Window setup
         self.app = QApplication([])
@@ -20,6 +22,27 @@ class pyChatClientBackend:
         self.ui = Ui_loginScreen_MainWindow()
         self.ui.setupUi(self.window)
         self.ui.currentDateTime_label.setText(self.current_datetime)
+
+        # Connection setup
+        self.pychat_client.connect()
+
+        # Connecting buttons to their respective functions
+        self.ui.login_pushButton.clicked.connect(self.login)
+        self.ui.registerButton.clicked.connect(self.register)
+        self.ui.forgotPasswordButton.clicked.connect(self.passwordReset)
+        self.ui.exitButton.clicked.connect(self.exit)
+
+    def login(self):
+        print("Inside of login")
+
+    def register(self):
+        print("Inside of register")
+
+    def passwordReset(self):
+        print("Inside of passwordReset")
+
+    def exit(self):
+        print("Inside of exit")
 
     def run(self):
         self.window.show()
